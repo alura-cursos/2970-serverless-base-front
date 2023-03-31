c2e41aa94acdcc19746a7187564303424c411857
JulianaAmoasei <late.parrot@gmail.com>
2023-03-31 14:41:28 -0300

    video: usando a URL no front

--- src/servicos/api-connect.js
+++ src/servicos/api-connect.js
@@ -1,3 +1,5 @@
+import geraUrlPreassinada from "./geradorUrlS3";
+
 const BASE_URL = "http://curso-serverless2-api-1428908743.us-east-1.elb.amazonaws.com";
 
 function buildFetchObj(metodo, contentType, body) {
@@ -23,4 +25,23 @@ async function criaRegistro(novoRegistro) {
   }
 }
 
-export { criaRegistro };
+async function enviaArquivoViaURL (url, arquivo) {
+  const fetchObj = buildFetchObj("PUT", "text/csv; charset=utf-8", arquivo);
+  try {
+    const res = await fetch(url, fetchObj);
+    if (res.status === 200) {
+      return "upload do arquivo concluído"
+    } else {
+      return "falha no upload"
+    }
+  } catch (erro) {
+    return erro;
+  }
+}
+
+async function geraPresignURL (nomeArquivo) {
+  const urlChave = await geraUrlPreassinada(nomeArquivo);
+  return urlChave;
+}
+
+export { criaRegistro, enviaArquivoViaURL, geraPresignURL };
